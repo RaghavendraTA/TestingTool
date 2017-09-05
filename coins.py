5bd9066d9e9358ef974015fef3a415cf305767d2
Raghavendra T.A <raghavendrathemaker@gmail.com>
2017-09-05 19:47:36 +0530

    Update coins.py

--- coins.py
+++ coins.py
@@ -1,15 +1,31 @@
 
+flag = True
+
 def coin_change(x, coins, S):
+    global flag
     ck = coins.pop(0)
     while x >= ck:
         x -= ck
         S.append(ck)
-    if x > 0:
-        for k in range(len(coins)):
-            coin_change(x, coins[k:], S.copy())
-    if x == 0:
+    for k in range(len(coins)):
+        coin_change(x, coins[k:], S.copy())
+    if x == 0 and flag:
+        flag = False
         print(S)
 
-C = [10, 2, 5]
+def greedy_coin_change(x, coins):
+    S = []
+    while x > 0 and len(coins) > 0:
+        ck = coins.pop(0)
+        while x >= ck:
+            x -= ck
+            S.append(ck)
+    if x == 0: print(S)
+    else: print("No solution")
+
+#C = list(map(int, input().strip().split()))
+n = int(input())
+C = [2, 5, 13, 100]
 C.sort(reverse=True)
-coin_change(9, C, [])
+coin_change(n, C, [])
+#greedy_coin_change(n, C)
