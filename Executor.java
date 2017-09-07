06c6bd617c43f4a81d1d66b3e132a08026694118
Raghavendra T.A <raghavendrathemaker@gmail.com>
2017-09-07 22:47:50 +0530

    Update Executor.java

--- Executor.java
+++ Executor.java
@@ -1,17 +1,9 @@
+package hackerearth;
+
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-
-class Sol implements Callable {
-
-	@Override
-	public String call() throws InterruptedException {
-		Thread.sleep(2000);
-		return "trial";
-	}
-}
 
 public class Executor implements Callable {
 
@@ -21,23 +13,17 @@ public class Executor implements Callable {
 		return "Hello";
 	}
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
 		
 		long startTime = System.nanoTime();
 		ExecutorService executor = Executors.newFixedThreadPool(5);
-		Callable<String> callable = new Solution();
-		Callable<String> callable2 = new Sol();
+		Callable<String> callable = () -> {
+			return "Hi";
+		};
 		Future<String> value = executor.submit(callable);
-		Future<String> value2 = executor.submit(callable2);
-		try {
-			System.out.println("The returned value is : " + value.get());
-			System.out.println("The returned value is : " + value2.get());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		System.out.println("The returned value is : " + value.get());
 		executor.shutdown();
-		long endTime = System.nanoTime();
-		System.out.println("Took " + ((endTime - startTime) / 1000000)+ " ms"); 
+		System.out.println("Took " + ((System.nanoTime() - startTime) / 1000000)+ " ms"); 
 	}
 
 }
