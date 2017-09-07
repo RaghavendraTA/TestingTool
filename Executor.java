2eda09687934e4de582727fbbb24f9e90b34d76c
Raghavendra T.A <raghavendrathemaker@gmail.com>
2017-09-07 22:37:06 +0530

    Update Executor.java

--- Executor.java
+++ Executor.java
@@ -4,26 +4,40 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+class Sol implements Callable {
+
+	@Override
+	public String call() throws InterruptedException {
+		Thread.sleep(2000);
+		return "trial";
+	}
+}
+
 public class Executor implements Callable {
 
 	@Override
 	public String call() throws InterruptedException {
-		Thread.sleep(5000);
+		Thread.sleep(1000);
 		return "Hello";
 	}
 
 	public static void main(String args[]) {
+		
+		long startTime = System.nanoTime();
 		ExecutorService executor = Executors.newFixedThreadPool(5);
 		Callable<String> callable = new Solution();
+		Callable<String> callable2 = new Sol();
 		Future<String> value = executor.submit(callable);
+		Future<String> value2 = executor.submit(callable2);
 		try {
 			System.out.println("The returned value is : " + value.get());
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} catch (ExecutionException e) {
+			System.out.println("The returned value is : " + value2.get());
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		executor.shutdown();
+		long endTime = System.nanoTime();
+		System.out.println("Took " + ((endTime - startTime) / 1000000)+ " ms"); 
 	}
 
 }
