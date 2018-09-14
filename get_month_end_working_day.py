2f17e8e8befbac4aaf14dc8848462c522488dedd
Raghavendra T.A <raghavendrathemaker@gmail.com>
2018-09-14 21:29:39 +0530

    Create get_month_end_working_day.py

---
+++ get_month_end_working_day.py
@@ -0,0 +1,14 @@
+import numpy as np
+import math
+from datetime import datetime, timedelta
+
+# Hello World program in Python
+
+def get_last_working_day_of_this_month():
+    today = datetime.now().strftime('%Y-%m')
+    next_month = np.datetime64(today) + np.timedelta64(1, 'M')
+    last_day = next_month - np.timedelta64(1, 'D')
+    return np.busday_offset(last_day, 0, roll='backward')
+
+temp = get_last_working_day_of_this_month()
+print(temp)
